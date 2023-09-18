19195960de613a1ad2d3a66ed4f708747ac63430
sasezaki <sasezaki@gmail.com>
2023-09-18 17:55:22 +0900

    Do phpstan test dir

--- test/Rules/ContextKeyPlaceHolderRuleTest.php
+++ test/Rules/ContextKeyPlaceHolderRuleTest.php
@@ -9,7 +9,7 @@ use PHPStan\Testing\RuleTestCase;
 use Sfp\PHPStan\Psr\Log\Rules\ContextKeyPlaceHolderRule;
 
 /**
- * @implements RuleTestCase<ContextKeyPlaceHolderRule>
+ * @extends RuleTestCase<ContextKeyPlaceHolderRule>
  * @covers \Sfp\PHPStan\Psr\Log\Rules\ContextKeyPlaceHolderRule
  */
 final class ContextKeyPlaceHolderRuleTest extends RuleTestCase
@@ -23,25 +23,25 @@ final class ContextKeyPlaceHolderRuleTest extends RuleTestCase
     public function testProcessNode(): void
     {
         $this->analyse([__DIR__ . '/data/contextKeyPlaceHolder.php'], [
-            'missing context'   => [
+            [
                 'Parameter $context of logger method Psr\Log\LoggerInterface::info() is required, when placeholder braces exists - {nonContext}',
-                17,
+                17, // missing context
             ],
-            'empty array'       => [
+            [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() has placeholder braces, but context key is not found against them. - {empty}',
-                18,
+                18, // empty array
             ],
-            'notMatched'        => [
+            [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() has placeholder braces, but context key is not found against them. - {notMatched}',
-                19,
+                19, // notMatched
             ],
-            'many placeholders' => [
+            [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() has placeholder braces, but context key is not found against them. - {notMatched1},{notMatched2}',
-                20,
+                20, // many placeholders
             ],
-            'log method'        => [
+            [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::log() has placeholder braces, but context key is not found against them. - {notMatched}',
-                21,
+                21, // log method
             ],
         ]);
     }
