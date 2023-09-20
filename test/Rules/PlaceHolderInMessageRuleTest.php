f093ba0cf5f1caaa3ef101e8dad7bfbd1bdcbbac
sasezaki <sasezaki@gmail.com>
2023-09-20 22:22:56 +0900

    Update test for tip()

--- test/Rules/PlaceHolderInMessageRuleTest.php
+++ test/Rules/PlaceHolderInMessageRuleTest.php
@@ -28,18 +28,22 @@ final class PlaceHolderInMessageRuleTest extends RuleTestCase
             [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() should not includes double braces. - {{doubleBrace}}',
                 15, // double braces
+                'See https://www.php-fig.org/psr/psr-3/#12-message',
             ],
             [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() has braces. But it includes invalid characters for placeholder. - { space }',
                 16, // invalid placeholder char
+                'See https://www.php-fig.org/psr/psr-3/#12-message',
             ],
             [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::log() has braces. But it includes invalid characters for placeholder. - {&invalid&}',
                 17, // call log() method
+                'See https://www.php-fig.org/psr/psr-3/#12-message',
             ],
             [
                 'Parameter $message of logger method Psr\Log\LoggerInterface::info() has braces. But it includes invalid characters for placeholder. - {&a},{&b},{&c}',
                 18, // Many Invalid PlaceHolders
+                'See https://www.php-fig.org/psr/psr-3/#12-message',
             ],
         ]);
     }
